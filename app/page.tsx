d2626bd6eade5c4f1170bf04ef42b531ba3ae717
AteronBD <atalayreal@gmail.com>
2026-02-05 13:54:44 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from 'react';
+import './globals.css';
 import { 
   Package, Truck, Bell, BarChart3, Building2, Clock, 
   AlertTriangle, CheckCircle, XCircle, ChevronRight, 
